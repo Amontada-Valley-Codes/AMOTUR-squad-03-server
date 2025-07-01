139d31f5252a91d4914ae0e78078c57ed9689670
Jonatas Marques <jonatasmarquessousa@gmail.com>
2025-07-01 16:34:05 -0300

    feat: create auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PlaceModule } from './place/place.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PlaceService } from './place/place.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PlaceModule, PrismaModule, UserModule],
+  imports: [PlaceModule, PrismaModule, UserModule, AuthModule],
   controllers: [],
   providers: []
 })
