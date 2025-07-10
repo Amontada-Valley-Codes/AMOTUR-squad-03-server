2d517d10a72e4c9c3aede41d936bf90ecfa6eead
Lianderson <liandersonr18@gmail.com>
2025-07-09 23:55:54 -0300

    coloca bcrypt no seed.js

--- seed.js
+++ seed.js
@@ -1,17 +1,20 @@
 // seed.js
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 
 const admin = process.env.ADMIN_INITIAL_EMAIL;
 const password = process.env.ADMIN_INITIAL_PASSWORD;
 
+const hashedpassword = bcrypt.hashSync(password, 10)
+        
 async function main() {
     await prisma.Users.upsert({
         where: { email: admin },
         update: {},
         create: {
             email: admin,
-            password: password,
+            password: hashedpassword,
             role: 'ADMIN',
         },
     });
