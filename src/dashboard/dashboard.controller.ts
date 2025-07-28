f9cedba9c95942deffc1b4b6a2ce95b57f273693
Lianderson <liandersonr18@gmail.com>
2025-07-28 00:32:16 -0300

    docs: documentação para a rota dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { AdminGuard } from 'src/auth/admin.guard';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
@@ -14,6 +14,10 @@ export class DashboardController {
     @ApiBearerAuth()
     @UseGuards(AdminGuard)
     @Get(':data')
+    @ApiOperation({ summary: 'Dados para dashboard' })
+    @ApiResponse({ status: 200, description: "Dados retornados com sucesso!!" })
+    @ApiParam({ name: 'data', type: String, description: 'data de hoje', example: "2025-07-30" })
+    @HttpCode(HttpStatus.OK)  
     async quantidade(@Param('data')data: string): Promise<{totalDeLugares: number, totalDeUsers: number, lugaresPorTipo:{ type: string; quantidade: number; }[], lugaresPorRegiao:{ location: string; quantidade: number; }[],contagemlogados:number}> {
         const totalDeLugares = await this.dashboardService.contarLugares()
         const totalDeUsers = await this.dashboardService.contarUsers()
